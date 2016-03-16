bdf2279b8f7bca675b2a0243a4f18f8e4e7ecac5
User Magnar <magnar@s0.mydevil.net>
2016-03-17 00:51:31 +0100

    Repaired urls

--- urls.py
+++ urls.py
@@ -9,10 +9,12 @@ from views import EventInfoView
 
 urlpatterns = [
     #url(r'^(?P<acronym>[A-Z]{4,11}\d{4}[A-Z]{2,3})/', FieldEventDetailsView.as_view()),
-    url(r'^kalendarz/(?P<year>\w+)?$', EventCalendarView.as_view()),
+    url(r'^kalendarz/$', EventCalendarView.as_view()),
+    url(r'^kalendarz/(?P<year>\w+)/$', EventCalendarView.as_view()),
     url(r'^kalendarz/archiwum/?$', ArchivalCalendarsView.as_view()),
     url(r'^wyniki/archiwum/?$', ArchivalResultsView.as_view()),
-    url(r'^wyniki/(?P<year>\w+)?/?$', EventsResultsView.as_view()),
+    url(r'^wyniki/$', EventsResultsView.as_view()),
+    url(r'^wyniki/(?P<year>\w+)/$', EventsResultsView.as_view()),
     url(r'^informacje-o-zawodach/(?P<year>\w+)?$', EventInfoView.as_view()),
     url(r'^(?P<slug>[-\w]+)/$', EventDetailView.as_view(), name='event-detail'),
 ]
